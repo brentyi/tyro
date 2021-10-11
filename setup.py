e50d9ac7d470288dd6528d43c3de400f30fbbf0e
Brent Yi <yibrenth@gmail.com>
2021-10-11 02:15:04 -0700

    Add Python 3.10

--- setup.py
+++ setup.py
@@ -26,4 +26,12 @@ setup(
             "mypy",
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
