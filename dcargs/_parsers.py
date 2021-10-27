94617e98240c666a822a005edf7dd83a6492c050
Brent Yi <yibrenth@gmail.com>
2021-10-27 15:53:06 -0700

    Minor refactor

--- dcargs/_parsers.py
+++ dcargs/_parsers.py
@@ -1,6 +1,6 @@
 import argparse
 import dataclasses
-from typing import Dict, List, Optional, Set, Tuple, Type, TypeVar, Union
+from typing import Any, Dict, List, Optional, Set, Tuple, Type, TypeVar, Union
 
 from typing_extensions import _GenericAlias  # type: ignore
 
@@ -13,7 +13,7 @@ T = TypeVar("T")
 class ParserDefinition:
     """Each parser contains a list of arguments and optionally a subparser."""
 
-    args: List["_arguments.ArgumentDefinition"]
+    args: List[_arguments.ArgumentDefinition]
     subparsers: Optional["SubparsersDefinition"]
 
     def apply(self, parser: argparse.ArgumentParser) -> None:
@@ -45,14 +45,10 @@ class ParserDefinition:
     def from_dataclass(
         cls: Union[Type[T], _GenericAlias],
         parent_dataclasses: Optional[Set[Type]],
-        subparser_name_from_type: Dict[Type, str],
         parent_type_from_typevar: Optional[Dict[TypeVar, Type]],
         default_instance: Optional[T],
     ) -> Tuple["ParserDefinition", _construction.ConstructionMetadata]:
         """Create a parser definition from a dataclass."""
-
-        # TODO: this function is getting bloated and can probably be refactored. There's
-        # also some repeated logic between here and _construct.py; worth revisiting.
 
         if parent_dataclasses is None:
             parent_dataclasses = set()
@@ -69,123 +65,56 @@ class ParserDefinition:
         assert (
             cls not in parent_dataclasses
         ), f"Found a cyclic dataclass dependency with type {cls}"
+        parent_dataclasses = parent_dataclasses | {cls}
 
         args = []
         subparsers = None
         metadata = _construction.ConstructionMetadata()
         for field in _resolver.resolved_fields(cls):  # type: ignore
+
+            # Ignore fields not in constructor
             if not field.init:
                 continue
 
-            # If set to False, we don't directly create an argument from this field.
-            arg_from_field: bool = True
-
-            # Resolve field type
-            field_type = (
-                type_from_typevar[field.type]  # type: ignore
-                if field.type in type_from_typevar
-                else field.type
+            field_parser = _NestedDataclassHandler(
+                cls,
+                field,
+                type_from_typevar,
+                parent_dataclasses,
+                default_instance,
             )
 
-            # Add arguments for nested dataclasses.
-            if _resolver.is_dataclass(field_type):
-                default = None
-                if default_instance is not None:
-                    default = getattr(default_instance, field.name)
-                elif field.default is not dataclasses.MISSING:
-                    default = field.default
+            # Try to create subparsers from this field.
+            subparsers_out = field_parser.handle_unions_over_dataclasses()
+            if subparsers_out is not None:
+                assert (
+                    subparsers is None
+                ), "Only one subparser (union over dataclasses) is allowed per class"
+                subparsers, subparsers_metadata = subparsers_out
+                metadata.update(subparsers_metadata)
 
-                child_definition, child_metadata = ParserDefinition.from_dataclass(
-                    field_type,
-                    parent_dataclasses | {cls},
-                    subparser_name_from_type=subparser_name_from_type,
-                    parent_type_from_typevar=type_from_typevar,
-                    default_instance=default,
-                )
+                continue
+
+            # Try to interpret field as a nested dataclass.
+            nested_out = field_parser.handle_nested_dataclasses()
+            if nested_out is not None:
+                child_args, child_metadata = nested_out
+                args.extend(child_args)
                 metadata.update(child_metadata)
 
-                child_args = child_definition.args
-                for i, arg in enumerate(child_args):
-                    child_args[i] = arg.prefix(
-                        field.name + _strings.NESTED_DATACLASS_DELIMETER
-                    )
+                continue
 
-                args.extend(child_args)
-
-                if child_definition.subparsers is not None:
-                    assert subparsers is None
-                    subparsers = child_definition.subparsers
-
-                metadata.role_from_field[
-                    field
-                ] = _construction.FieldRole.NESTED_DATACLASS
-                arg_from_field = False
-
-            # Union of dataclasses should create subparsers.
-            if hasattr(field_type, "__origin__") and field_type.__origin__ is Union:
-                # We don't use sets here to retain order of subcommands.
-                options = field_type.__args__
-                options = map(
-                    lambda x: x if x not in type_from_typevar else type_from_typevar[x],
-                    field_type.__args__,
-                )
-                options_no_none = [o for o in options if o != type(None)]  # noqa
-                if len(options_no_none) >= 2 and all(
-                    map(_resolver.is_dataclass, options_no_none)
-                ):
-                    assert (
-                        subparsers is None
-                    ), "Only one subparser group is supported per dataclass"
-                    assert (
-                        field.default == dataclasses.MISSING
-                    ), "Default dataclass value not yet supported for subparser definitions"
-
-                    parsers: Dict[str, ParserDefinition] = {}
-                    for option in options_no_none:
-                        subparser_name = (
-                            subparser_name_from_type[option]
-                            if option in subparser_name_from_type
-                            else _strings.hyphen_separated_from_camel_case(
-                                option.__name__
-                            )
-                        )
-                        metadata.subparser_name_from_type[option] = subparser_name
-
-                        (
-                            parsers[subparser_name],
-                            child_metadata,
-                        ) = ParserDefinition.from_dataclass(
-                            option,
-                            parent_dataclasses | {cls},
-                            subparser_name_from_type,
-                            parent_type_from_typevar=type_from_typevar,
-                            default_instance=None,
-                        )
-                        metadata.update(child_metadata)
-
-                    subparsers = SubparsersDefinition(
-                        name=field.name,
-                        description=_docstrings.get_field_docstring(cls, field.name),
-                        parsers=parsers,
-                        required=(
-                            options == options_no_none
-                        ),  # Not required if no options.
-                    )
-                    metadata.role_from_field[field] = _construction.FieldRole.SUBPARSERS
-                    arg_from_field = False
-
-            # Make an argument!
-            if arg_from_field:
-                arg, role = _arguments.ArgumentDefinition.make_from_field(
-                    cls,
-                    field,
-                    type_from_typevar,
-                    default_override=getattr(default_instance, field.name)
-                    if default_instance is not None
-                    else None,
-                )
-                args.append(arg)
-                metadata.role_from_field[field] = role
+            # Handle simple fields!
+            arg, role = _arguments.ArgumentDefinition.make_from_field(
+                cls,
+                field,
+                type_from_typevar,
+                default_override=getattr(default_instance, field.name)
+                if default_instance is not None
+                else None,
+            )
+            args.append(arg)
+            metadata.role_from_field[field] = role
 
         return (
             ParserDefinition(
@@ -204,3 +133,115 @@ class SubparsersDefinition:
     description: Optional[str]
     parsers: Dict[str, ParserDefinition]
     required: bool
+
+
+@dataclasses.dataclass
+class _NestedDataclassHandler:
+    """Helper functions for handling nested dataclasses, which are converted to either
+    subparsers or prefixed fields."""
+
+    cls: Type
+    field: dataclasses.Field
+    type_from_typevar: Dict[TypeVar, Type]
+    parent_dataclasses: Set[Type]
+    default_instance: Any
+
+    def handle_unions_over_dataclasses(
+        self,
+    ) -> Optional[Tuple["SubparsersDefinition", _construction.ConstructionMetadata]]:
+        """Handle unions over dataclasses, which are converted to subparsers.. Returns
+        `None` if not applicable."""
+
+        metadata = _construction.ConstructionMetadata()
+
+        # Union of dataclasses should create subparsers.
+        if (
+            not hasattr(self.field.type, "__origin__")
+            or self.field.type.__origin__ is not Union
+        ):
+            return None
+
+            # We don't use sets here to retain order of subcommands.
+        options = self.field.type.__args__
+        options = map(
+            lambda x: x
+            if x not in self.type_from_typevar
+            else self.type_from_typevar[x],
+            self.field.type.__args__,
+        )
+        options_no_none = [o for o in options if o != type(None)]  # noqa
+        if len(options_no_none) < 2 or not all(
+            map(_resolver.is_dataclass, options_no_none)
+        ):
+            return None
+
+        assert (
+            self.field.default == dataclasses.MISSING
+        ), "Default dataclass value not yet supported for subparser definitions"
+
+        parsers: Dict[str, ParserDefinition] = {}
+        for option in options_no_none:
+            subparser_name = _strings.hyphen_separated_from_camel_case(option.__name__)
+            metadata.subparser_name_from_type[option] = subparser_name
+
+            (
+                parsers[subparser_name],
+                child_metadata,
+            ) = ParserDefinition.from_dataclass(
+                option,
+                self.parent_dataclasses,
+                parent_type_from_typevar=self.type_from_typevar,
+                default_instance=None,
+            )
+            metadata.update(child_metadata)
+
+        subparsers = SubparsersDefinition(
+            name=self.field.name,
+            description=_docstrings.get_field_docstring(self.cls, self.field.name),
+            parsers=parsers,
+            required=(options == options_no_none),  # Not required if no options.
+        )
+        metadata.role_from_field[self.field] = _construction.FieldRole.SUBPARSERS
+
+        return subparsers, metadata
+
+    def handle_nested_dataclasses(
+        self,
+    ) -> Optional[
+        Tuple[List[_arguments.ArgumentDefinition], _construction.ConstructionMetadata]
+    ]:
+        """Handle nested dataclasses. Returns `None` if not applicable."""
+        # Resolve field type
+        field_type = (
+            self.type_from_typevar[self.field.type]  # type: ignore
+            if self.field.type in self.type_from_typevar  # type: ignore
+            else self.field.type
+        )
+        if not _resolver.is_dataclass(field_type):
+            return None
+
+        # Add arguments for nested dataclasses.
+        default = None
+        if self.default_instance is not None:
+            default = getattr(self.default_instance, self.field.name)
+        elif self.field.default is not dataclasses.MISSING:
+            default = self.field.default
+
+        child_definition, child_metadata = ParserDefinition.from_dataclass(
+            field_type,
+            self.parent_dataclasses,
+            parent_type_from_typevar=self.type_from_typevar,
+            default_instance=default,
+        )
+
+        child_args = child_definition.args
+        for i, arg in enumerate(child_args):
+            child_args[i] = arg.prefix(
+                self.field.name + _strings.NESTED_DATACLASS_DELIMETER
+            )
+
+        child_metadata.role_from_field[
+            self.field
+        ] = _construction.FieldRole.NESTED_DATACLASS
+
+        return child_args, child_metadata
