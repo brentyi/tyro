3b18c8f157f5313adfc0d21df44031e95f4e7cf0
Brent Yi <yibrenth@gmail.com>
2022-07-21 15:11:34 -0700

    Add py.typed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dcargs",
-    version="0.1.9",
+    version="0.1.10",
     description="Strongly typed, zero-effort CLIs",
     long_description=long_description,
     long_description_content_type="text/markdown",
