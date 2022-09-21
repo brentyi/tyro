59f6ae6126572ecd59ca77e377e911d22ea58952
Brent Yi <yibrenth@gmail.com>
2022-09-20 22:37:51 -0700

    Ignore flax tests for Python 3.10

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,8 @@ collect_ignore_glob = []
 if sys.version_info.major == 3 and sys.version_info.minor == 7:
     collect_ignore_glob.append("*_ignore_py37.py")
 
+if sys.version_info.major == 3 and sys.version_info.minor == 10:
+    collect_ignore_glob.append("*_ignore_py310.py")
+
 if not (sys.version_info.major == 3 and sys.version_info.minor == 10):
     collect_ignore_glob.append("*_only_py310.py")
