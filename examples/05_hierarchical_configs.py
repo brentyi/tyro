11930b5f65bec5bbc9ee0ceb23fcee1d3cdf1f56
Brent Yi <yibrenth@gmail.com>
2022-09-21 01:31:06 -0700

    Minor docs fixes

--- examples/05_hierarchical_configs.py
+++ examples/05_hierarchical_configs.py
@@ -34,7 +34,7 @@ class OptimizerConfig:
 @dataclasses.dataclass
 class ExperimentConfig:
     # Various configurable options for our optimizer.
-    optimizer_config: OptimizerConfig
+    optimizer: OptimizerConfig
 
     # Batch size.
     batch_size: int = 32
