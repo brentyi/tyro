brentyi/tyro

commit 99f73e14b3e54188b1b0a58d72ed365ac4e3de80

Test coverage improvements

tyro/_parsers.py
@@ -40,7 +40,6 @@ class ParserSpecification:
         f: Callable[..., T],
         description: Optional[str],
         parent_classes: Set[Type],
-        parent_type_from_typevar: Optional[Dict[TypeVar, Type]],
         default_instance: Union[
             T, _fields.PropagatingMissingType, _fields.NonpropagatingMissingType
         ],
@@ -52,10 +51,6 @@ class ParserSpecification:
         # Resolve generic types.
         f, type_from_typevar = _resolver.resolve_generic_types(f)
         f = _resolver.narrow_type(f, default_instance)
-        if parent_type_from_typevar is not None:
-            for typevar, typ in type_from_typevar.items():
-                if typ in parent_type_from_typevar:
-                    type_from_typevar[typevar] = parent_type_from_typevar[typ]  # type: ignore
 
         # Cycle detection.
         #
@@ -135,7 +130,6 @@ class ParserSpecification:
                         field.typ,
                         description=None,
                         parent_classes=parent_classes,
-                        parent_type_from_typevar=type_from_typevar,
                         default_instance=field.default,
                         prefix=_strings.make_field_name([prefix, field.name]),
                         subcommand_prefix=subcommand_prefix,
@@ -238,13 +232,7 @@ class ParserSpecification:
                 arg.add_argument(positional_group)
                 continue
 
-            if arg.prefix in group_from_prefix:
-                arg.add_argument(group_from_prefix[arg.prefix])
-            else:
-                # Suppressed argument: still need to add them, but they won't show up in
-                # the helptext so it doesn't matter which group.
-                assert arg.lowered.help is argparse.SUPPRESS
-                arg.add_argument(group_from_prefix[""])
+            arg.add_argument(group_from_prefix[arg.prefix])
 
         # Create subparser tree.
         if len(self.subparsers_from_name) > 0:
@@ -393,7 +381,6 @@ class SubparsersSpecification:
                 else option,
                 description=subcommand_config.description,
                 parent_classes=parent_classes,
-                parent_type_from_typevar=type_from_typevar,
                 default_instance=subcommand_config.default,
                 prefix=prefix,
                 subcommand_prefix=prefix,

tests/test_errors.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import List, Tuple, Union
+from typing import List, Tuple, TypeVar, Union
 
 import pytest
 
@@ -93,6 +93,24 @@ def test_missing_annotation_1():
 
 def test_missing_annotation_2():
     def main(*, a) -> None:
+        pass
+
+    with pytest.raises(tyro.UnsupportedTypeAnnotationError):
+        tyro.cli(main, args=["--help"])
+
+
+def test_tuple_needs_default():
+    def main(arg: tuple) -> None:  # type: ignore
+        pass
+
+    with pytest.raises(tyro.UnsupportedTypeAnnotationError):
+        tyro.cli(main, args=["--help"])
+
+
+def test_unbound_typevar():
+    T = TypeVar("T")
+
+    def main(arg: T) -> None:  # type: ignore
         pass
 
     with pytest.raises(tyro.UnsupportedTypeAnnotationError):

.github/workflows/coverage.yml
@@ -11,10 +11,10 @@ jobs:
     runs-on: ubuntu-latest
     steps:
       - uses: actions/checkout@v2
-      - name: Set up Python 3.8
+      - name: Set up Python 3.10
         uses: actions/setup-python@v4
         with:
-          python-version: 3.8
+          python-version: "3.10"
       - name: Install dependencies
         run: |
           curl -sSL https://install.python-poetry.org | python3 -

pyproject.toml
@@ -72,4 +72,7 @@ exclude_lines = [
 
     # or fallback imports
     "except ImportError:",
+
+    # or anything that's deprecated
+    "deprecated",
 ]

tyro/_cli.py
@@ -276,7 +276,6 @@ def _cli_impl(
         f,
         description=description,
         parent_classes=set(),  # Used for recursive calls.
-        parent_type_from_typevar=None,  # Used for recursive calls.
         default_instance=default_instance_internal,  # Overrides for default values.
         prefix="",  # Used for recursive calls.
     )

tyro/_instantiators.py
@@ -104,13 +104,6 @@ def instantiator_from_type(
     - A metadata structure, which specifies parameters for argparse.
     """
 
-    # Resolve typevars.
-    if typ in type_from_typevar:
-        return instantiator_from_type(
-            type_from_typevar[typ],  # type: ignore
-            type_from_typevar,
-        )
-
     # Handle Any.
     if typ is Any:
         raise UnsupportedTypeAnnotationError("`Any` is not a parsable type.")

commit 245b47bdb4af78ef71f58bee50ebc4bfb026f25d

Fix error when all arguments in a group are suppressed - Regression in https://github.com/brentyi/tyro/commit/99f73e14b3e54188b1b0a58d72ed365ac4e3de80 - Reported in https://github.com/nerfstudio-project/nerfstudio/issues/882 - Closes #18

tyro/_fields.py
@@ -57,6 +57,14 @@ class FieldDefinition:
     # user-facing argument name doesn't match the keyword expected by our callable.
     name_override: Optional[Any]
 
+    def __post_init__(self):
+        if (
+            _markers.Fixed in self.markers or _markers.Suppress in self.markers
+        ) and self.default in MISSING_SINGLETONS:
+            raise _instantiators.UnsupportedTypeAnnotationError(
+                f"Field {self.name} is missing a default value!"
+            )
+
     @staticmethod
     def make(
         name: str,

tyro/_parsers.py
@@ -232,7 +232,13 @@ class ParserSpecification:
                 arg.add_argument(positional_group)
                 continue
 
-            arg.add_argument(group_from_prefix[arg.prefix])
+            if arg.prefix in group_from_prefix:
+                arg.add_argument(group_from_prefix[arg.prefix])
+            else:
+                # Suppressed argument: still need to add them, but they won't show up in
+                # the helptext so it doesn't matter which group.
+                assert arg.lowered.help is argparse.SUPPRESS
+                arg.add_argument(group_from_prefix[""])
 
         # Create subparser tree.
         if len(self.subparsers_from_name) > 0:

tyro/_resolver.py
@@ -19,7 +19,6 @@ from typing import (
     cast,
 )
 
-from attr import dataclass
 from typing_extensions import Annotated, get_args, get_origin, get_type_hints
 
 TypeOrCallable = TypeVar("TypeOrCallable", Type, Callable)

tyro/conf/_markers.py
@@ -18,6 +18,9 @@ Positional = Annotated[T, None]
 """A type `T` can be annotated as `Positional[T]` if we want to parse it as a positional
 argument."""
 
+# TODO: the verb tenses here are inconsistent, naming could be revisited.
+# Perhaps Suppress should be Suppressed? But SuppressedFixed would be weird.
+
 Fixed = Annotated[T, None]
 """A type `T` can be annotated as `Fixed[T]` to prevent `tyro.cli` from parsing it; a
 default value should be set instead. Note that fields with defaults that can't be parsed

tests/test_conf.py
@@ -401,3 +401,54 @@ def test_fixed_recursive():
             args=["--x", "True"],
             default=A(False),
         ) == A(True)
+
+
+def test_suppressed_group():
+    """Reproduction of https://github.com/nerfstudio-project/nerfstudio/issues/882."""
+
+    @dataclasses.dataclass
+    class Inner:
+        a: int
+        b: int
+
+    def main(
+        value: int,
+        inner: tyro.conf.Suppress[Inner] = Inner(1, 2),
+    ) -> int:
+        return value + inner.a + inner.b
+
+    assert tyro.cli(main, args=["--value", "5"]) == 8
+
+
+def test_fixed_group():
+    """Reproduction of https://github.com/nerfstudio-project/nerfstudio/issues/882."""
+
+    @dataclasses.dataclass
+    class Inner:
+        a: int
+        b: int
+
+    def main(
+        value: int,
+        inner: tyro.conf.Fixed[Inner] = Inner(1, 2),
+    ) -> int:
+        return value + inner.a + inner.b
+
+    assert tyro.cli(main, args=["--value", "5"]) == 8
+
+
+def test_fixed_suppressed_group():
+    """Reproduction of https://github.com/nerfstudio-project/nerfstudio/issues/882."""
+
+    @dataclasses.dataclass
+    class Inner:
+        a: int
+        b: int
+
+    def main(
+        value: int,
+        inner: tyro.conf.Fixed[Inner] = Inner(1, 2),
+    ) -> int:
+        return value + inner.a + inner.b
+
+    assert tyro.cli(main, args=["--value", "5"]) == 8

tests/test_errors.py
@@ -115,3 +115,19 @@ def test_unbound_typevar():
 
     with pytest.raises(tyro.UnsupportedTypeAnnotationError):
         tyro.cli(main, args=["--help"])
+
+
+def test_missing_default_fixed():
+    def main(value: tyro.conf.SuppressFixed[tyro.conf.Fixed[int]]) -> int:
+        return value
+
+    with pytest.raises(tyro.UnsupportedTypeAnnotationError):
+        tyro.cli(main, args=["--help"])
+
+
+def test_missing_default_suppressed():
+    def main(value: tyro.conf.Suppress[int]) -> int:
+        return value
+
+    with pytest.raises(tyro.UnsupportedTypeAnnotationError):
+        tyro.cli(main, args=["--help"])