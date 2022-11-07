5aabacbddcc07f6fbbf2338b5bb24945c9255425
Brent Yi <yibrenth@gmail.com>
2022-11-07 05:43:42 -0800

    PEP 585 support in Python 3.9 (addresses #19)

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,5 +7,8 @@ if sys.version_info.major == 3 and sys.version_info.minor == 7:
 if sys.version_info.major == 3 and sys.version_info.minor == 10:
     collect_ignore_glob.append("*_ignore_py310.py")
 
+if not (sys.version_info.major == 3 and sys.version_info.minor >= 9):
+    collect_ignore_glob.append("*_above_py39.py")
+
 if not (sys.version_info.major == 3 and sys.version_info.minor == 10):
     collect_ignore_glob.append("*_only_py310.py")
