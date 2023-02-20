76447796125ddad2b8e244ff857907c19a6b0dff
Brent Yi <yibrenth@gmail.com>
2023-02-20 14:49:35 -0800

    Patch for 3.7

--- tyro/_subcommand_matching.py
+++ tyro/_subcommand_matching.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
 import dataclasses
-from typing import Any, Callable, Dict, Optional, Tuple, Union, get_args, get_origin
+from typing import Any, Callable, Dict, Optional, Tuple, Union
+
+from typing_extensions import get_args, get_origin
 
 from . import _fields, _instantiators, _resolver, _typing
 from .conf import _confstruct
