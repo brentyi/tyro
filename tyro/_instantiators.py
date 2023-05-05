8fb2d7071f926af6c5f6aabed2bfb7dfdd95c30e
Brent Yi <yibrenth@gmail.com>
2023-05-05 01:00:22 -0700

    Format

--- tyro/_instantiators.py
+++ tyro/_instantiators.py
@@ -486,8 +486,7 @@ def _instantiator_from_union(
                 )
         raise ValueError(
             f"no type in {options} could be instantiated from"
-            f" {strings}.\n\nGot errors:  \n- "
-            + "\n- ".join(errors)
+            f" {strings}.\n\nGot errors:  \n- " + "\n- ".join(errors)
         )
 
     return union_instantiator, InstantiatorMetadata(
