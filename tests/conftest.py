89127bbdffe989b0ed10ae6b5a79b79a49a81195
Brent Yi <yibrenth@gmail.com>
2023-07-27 22:28:44 -0700

    Rework test run conditions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,12 @@
 import sys
 
 collect_ignore_glob = []
-if sys.version_info.major == 3 and sys.version_info.minor == 7:
-    collect_ignore_glob.append("*_ignore_py37.py")
 
-if sys.version_info.major == 3 and sys.version_info.minor == 10:
-    collect_ignore_glob.append("*_ignore_py310.py")
+if not (sys.version_info.major == 3 and sys.version_info.minor >= 8):
+    collect_ignore_glob.append("*_min_py38.py")
 
 if not (sys.version_info.major == 3 and sys.version_info.minor >= 9):
-    collect_ignore_glob.append("*_above_py39.py")
+    collect_ignore_glob.append("*_min_py39.py")
 
-if not (sys.version_info.major == 3 and sys.version_info.minor == 10):
-    collect_ignore_glob.append("*_only_py310.py")
+if not (sys.version_info.major == 3 and sys.version_info.minor >= 10):
+    collect_ignore_glob.append("*_min_py310.py")
