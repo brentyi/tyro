681ea7668540ca5a47d6e49c2e343c0eb4202ba9
Brent Yi <yibrenth@gmail.com>
2023-12-12 12:52:33 +0000

    Improve PEP 585/604 annotation errors for older versions of Python

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,14 @@ import sys
 
 collect_ignore_glob = []
 
-if not (sys.version_info.major == 3 and sys.version_info.minor >= 8):
+if not sys.version_info >= (3, 8):
     collect_ignore_glob.append("*_min_py38.py")
 
-if not (sys.version_info.major == 3 and sys.version_info.minor >= 9):
+if not sys.version_info >= (3, 9):
     collect_ignore_glob.append("*_min_py39.py")
 
-if not (sys.version_info.major == 3 and sys.version_info.minor >= 10):
+if not sys.version_info >= (3, 10):
     collect_ignore_glob.append("*_min_py310.py")
 
-if not (sys.version_info.major == 3 and sys.version_info.minor >= 11):
+if not sys.version_info >= (3, 11):
     collect_ignore_glob.append("test_py311_generated/*.py")
--- tests/test_py311_generated/test_conf_generated.py
+++ tests/test_py311_generated/test_conf_generated.py
@@ -164,9 +164,10 @@ def test_subparser_in_nested_with_metadata() -> None:
 
     @dataclasses.dataclass
     class Nested2:
-        subcommand: Annotated[
-            A, tyro.conf.subcommand("command-a", default=A(7))
-        ] | Annotated[B, tyro.conf.subcommand("command-b", default=B(9))]
+        subcommand: (
+            Annotated[A, tyro.conf.subcommand("command-a", default=A(7))]
+            | Annotated[B, tyro.conf.subcommand("command-b", default=B(9))]
+        )
 
     @dataclasses.dataclass
     class Nested1:
@@ -271,9 +272,10 @@ def test_subparser_in_nested_with_metadata_generic_alt() -> None:
 
     @dataclasses.dataclass
     class Nested2(Generic[T]):
-        subcommand: Annotated[
-            T, tyro.conf.subcommand("command-a", default=A(7))
-        ] | Annotated[B, tyro.conf.subcommand("command-b", default=B(9))]
+        subcommand: (
+            Annotated[T, tyro.conf.subcommand("command-a", default=A(7))]
+            | Annotated[B, tyro.conf.subcommand("command-b", default=B(9))]
+        )
 
     @dataclasses.dataclass
     class Nested1:
@@ -325,11 +327,11 @@ def test_subparser_in_nested_with_metadata_default_matching() -> None:
 
     @dataclasses.dataclass
     class Nested:
-        subcommand: Annotated[
-            B, tyro.conf.subcommand("one", default=default_one)
-        ] | Annotated[B, tyro.conf.subcommand("two")] | Annotated[
-            B, tyro.conf.subcommand("three", default=default_three)
-        ]
+        subcommand: (
+            Annotated[B, tyro.conf.subcommand("one", default=default_one)]
+            | Annotated[B, tyro.conf.subcommand("two")]
+            | Annotated[B, tyro.conf.subcommand("three", default=default_three)]
+        )
 
     # Match by hash.
     def main_one(x: Nested = Nested(default_one)) -> None:
