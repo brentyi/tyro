5b9d8c6c3ed5ad4b0a23ecc9b7e05e20f921989a
Brent Yi <yibrenth@gmail.com>
2024-05-10 12:29:12 +0100

    Update version-dependent ignore

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,15 +5,19 @@ collect_ignore_glob: List[str] = []
 
 if not sys.version_info >= (3, 8):
     collect_ignore_glob.append("*_min_py38.py")
+    collect_ignore_glob.append("*_min_py38_generated.py")
 
 if not sys.version_info >= (3, 9):
     collect_ignore_glob.append("*_min_py39.py")
+    collect_ignore_glob.append("*_min_py39_generated.py")
 
 if not sys.version_info >= (3, 10):
     collect_ignore_glob.append("*_min_py310.py")
+    collect_ignore_glob.append("*_min_py310_generated.py")
 
 if not sys.version_info >= (3, 12):
     collect_ignore_glob.append("*_min_py312.py")
+    collect_ignore_glob.append("*_min_py312_generated.py")
 
 if not sys.version_info >= (3, 11):
     collect_ignore_glob.append("test_py311_generated/*.py")
