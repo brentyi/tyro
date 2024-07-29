8aefc7369edb19b099c6ded4a2107e1ff7b645a9
Brent Yi <yibrenth@gmail.com>
2024-07-28 20:22:45 -0700

    Docs wording nit

--- examples/01_basics/03_dataclasses_defaults.py
+++ examples/01_basics/03_dataclasses_defaults.py
@@ -7,10 +7,11 @@ types.
 .. warning::
     We advise against mutation of configuration objects from a dataclass's
     :code:`__post_init__` method [#f1]_. In the example below,
-    :code:`__post_init__` would be called for both the :code:`Args()` object
-    provided as a default value and as an output from :func:`tyro.cli()`.
-    Instead, we show below one example of how derived fields can be defined
-    immutably.
+    :code:`__post_init__` would be called twice: once for the :code:`Args()`
+    object provided as a default value and another time for the :code:`Args()`
+    objected instantiated by :func:`tyro.cli()`. This can cause confusing
+    behavior! Instead, we show below one example of how derived fields can be
+    defined immutably.
 
     .. [#f1] Official Python docs for ``__post_init__`` can be found `here <https://docs.python.org/3/library/dataclasses.html#dataclasses.__post_init__>`_.
 
