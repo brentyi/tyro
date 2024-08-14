be74b1fec9d6e99205eafd451a24d8182083bdcd
Brent Yi <yibrenth@gmail.com>
2024-08-14 02:14:27 -0700

    Docs typo

--- examples/03_config_systems/02_overriding_yaml.py
+++ examples/03_config_systems/02_overriding_yaml.py
@@ -1,8 +1,8 @@
 """Overriding YAML Configs
 
 If you have a library of existing YAML files that you want to use, `tyro` can
-be used to override values in them. This example uses a dictionary; we
-generally recommend dataset configs for new projects.
+be used to override values in them. We generally recommend dataclass configs
+for new projects.
 
 Usage:
 `python ./02_overriding_yaml.py --help`
