7ed52b4c2711596d3034ccf6d5f1621327c60ea1
brentyi <yibrenth@gmail.com>
2024-10-10 14:09:31 -0700

    Switch to class, tests

--- examples/04_additional/15_decorator_subcommands.py
+++ examples/04_additional/15_decorator_subcommands.py
@@ -5,16 +5,21 @@ decorator-based API for subcommands, which is inspired by `click
 <https://click.palletsprojects.com/>`_.
 
 Usage:
-`python my_script.py greet Alice`
-`python my_script.py greet Bob --loud`
-`python my_script.py add 5 3`
+`python my_script.py --help`
+`python my_script.py greet --help`
+`python my_script.py greet --name Alice`
+`python my_script.py greet --name Bob --loud`
+`python my_script.py addition --help`
+`python my_script.py addition --a 5 --b 3`
 """
 
-from tyro.extras import app
+from tyro.extras import SubcommandApp
+
+app = SubcommandApp()
 
 
-@app.command()
-def greet(name: str, loud: bool = False):
+@app.command
+def greet(name: str, loud: bool = False) -> None:
     """Greet someone."""
     greeting = f"Hello, {name}!"
     if loud:
@@ -22,8 +27,8 @@ def greet(name: str, loud: bool = False):
     print(greeting)
 
 
-@app.command()
-def add(a: int, b: int):
+@app.command(name="addition")
+def add(a: int, b: int) -> None:
     """Add two numbers."""
     print(f"{a} + {b} = {a + b}")
 
--- src/tyro/extras/__init__.py
+++ src/tyro/extras/__init__.py
@@ -4,7 +4,6 @@ Compared to the core interface, APIs here are more likely to be changed or depre
 
 from .._argparse_formatter import set_accent_color as set_accent_color
 from .._cli import get_parser as get_parser
-from . import app as app
 from ._base_configs import overridable_config_cli as overridable_config_cli
 from ._base_configs import (
     subcommand_type_from_defaults as subcommand_type_from_defaults,
@@ -12,6 +11,7 @@ from ._base_configs import (
 from ._choices_type import literal_type_from_choices as literal_type_from_choices
 from ._serialization import from_yaml as from_yaml
 from ._serialization import to_yaml as to_yaml
+from ._subcommand_app import SubcommandApp as SubcommandApp
 from ._subcommand_cli_from_dict import (
     subcommand_cli_from_dict as subcommand_cli_from_dict,
 )
