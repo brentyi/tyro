e22cfd8ddd2bc02cd4da97b238c547ad0233f905
brentyi <yibrenth@gmail.com>
2024-11-03 23:29:55 -0800

    exports

--- src/tyro/constructors/__init__.py
+++ src/tyro/constructors/__init__.py
@@ -4,3 +4,6 @@ from ._primitive_spec import (
     UnsupportedTypeAnnotationError as UnsupportedTypeAnnotationError,
 )
 from ._registry import ConstructorRegistry as ConstructorRegistry
+from ._struct_spec import StructConstructorSpec as StructConstructorSpec
+from ._struct_spec import StructFieldSpec as StructFieldSpec
+from ._struct_spec import StructTypeInfo as StructTypeInfo
