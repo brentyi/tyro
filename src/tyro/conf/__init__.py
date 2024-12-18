38c201561d128f8a26e970756f7d8fef8f8c8ad0
brentyi <yibrenth@gmail.com>
2024-12-17 17:10:11 -0800

    `0.9.3`

--- src/tyro/conf/__init__.py
+++ src/tyro/conf/__init__.py
@@ -1,9 +1,12 @@
 """The :mod:`tyro.conf` submodule contains helpers for attaching parsing-specific
-configuration metadata to types via [PEP 593](https://peps.python.org/pep-0593/) runtime
+configuration metadata to types via `PEP 593 <https://peps.python.org/pep-0593/>`_ runtime
 annotations.
 
-Configuration flags are applied recursively, and should generally be subscripted:
-``Fixed[T]``, ``Suppress[T]``, etc.
+Flags will be applied recursively, and can be used one of multiple ways:
+
+1. They can be subscripted: ``tyro.conf.FlagConversionoff[bool]``.
+2. They can be passed into :py:data:`typing.Annotated`: ``Annotated[str, tyro.conf.FlagConversionOff]``.
+3. They can be passed into :func:`tyro.cli`: ``tyro.cli(Args, config=(tyro.conf.FlagConversionOff,))``.
 
 Features here are supported, but generally unnecessary and should be used sparingly.
 """
