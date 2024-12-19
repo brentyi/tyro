6e9a049f5bcda7da353ae357216e21493752635c
Brent Yi <yibrenth@gmail.com>
2024-12-19 10:54:15 -0800

    Don't assume object hashes are unique in caching logic (#215)
    * Fix cache collision edge cases
    
    * Fuzzy cache collision check
    
    * Remove old cache tests
    
    * Comments
    
    * Python 3.7
    
    * Add back cache test (for coverage)
    
    * More correct cache key
    
    * relax cache assert equality

--- tests/test_unsafe_cache.py
+++ tests/test_unsafe_cache.py
@@ -9,23 +9,24 @@ def test_unsafe_cache():
         nonlocal x
         x += 1
 
+    # >= is because of fuzz testing inside of unsafe_cache
     f(0)
     f(0)
     f(0)
-    assert x == 1
+    assert x >= 1
     f(1)
     f(1)
     f(1)
-    assert x == 2
+    assert x >= 2
     f(0)
     f(0)
     f(0)
-    assert x == 2
+    assert x >= 2
     f(2)
     f(2)
     f(2)
-    assert x == 3
+    assert x >= 3
     f(0)
     f(0)
     f(0)
-    assert x == 4
+    assert x >= 4
