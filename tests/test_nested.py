b1b94619193ed9c96f95af3974e48263d4e0d67a
brentyi <yibrenth@gmail.com>
2025-01-06 12:31:56 -0800

    Union.__getitem__

--- tests/test_nested.py
+++ tests/test_nested.py
@@ -1358,7 +1358,7 @@ def test_subcommand_default_with_conf_annotation() -> None:
             Annotated[SGDConfig, tyro.conf.subcommand(name="sgd")],
             Annotated[AdamConfig, tyro.conf.subcommand(name="adam")],
         ]
-        return Union[*cfgs]  # type: ignore
+        return Union.__getitem__(tuple(cfgs))  # type: ignore
 
     @dataclasses.dataclass(frozen=True)
     class Config1:
