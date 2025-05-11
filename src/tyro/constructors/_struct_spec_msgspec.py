069cac9850c95197b93dd498ddad1b53be296bab
Apoorv Khandelwal <mail@apoorvkh.com>
2025-05-10 22:14:24 -0400

    Integration for `msgspec` (#293)
    * Initial integration for msgspec.Struct
    
    * added msgspec tests
    
    * added example file for msgspec
    
    * update whats_supported docs with ml_collections, msgspec structs
    
    * ruff
    
    * Add some tests
    
    * Generics + subcommand tests for msgspec
    
    * Run `update_example_docs.py`
    
    ---------
    
    Co-authored-by: brentyi <yibrenth@gmail.com>

---
+++ src/tyro/constructors/_struct_spec_msgspec.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import sys
+
+from typing_extensions import get_type_hints
+
+from .._docstrings import get_field_docstring
+from .._singleton import MISSING, MISSING_NONPROP
+from ._struct_spec import StructConstructorSpec, StructFieldSpec, StructTypeInfo
+
+
+def msgspec_rule(info: StructTypeInfo) -> StructConstructorSpec | None:
+    if "msgspec" not in sys.modules.keys():  # pragma: no cover
+        return None
+
+    import msgspec
+
+    try:
+        if not issubclass(info.type, msgspec.Struct):
+            return None
+    except TypeError:  # issubclass failed
+        return None
+
+    # Handle msgspec struct objects.
+    field_list = []
+    struct_type = msgspec.inspect.type_info(info.type)
+    assert isinstance(struct_type, msgspec.inspect.StructType)
+
+    # We need to use the original type hints, because `field.type` returns
+    # a msgspec-specified type descriptor.
+    annotations = get_type_hints(info.type, include_extras=True)
+
+    for field in struct_type.fields:
+        if info.default not in (
+            MISSING,
+            MISSING_NONPROP,
+        ):
+            default = getattr(info.default, field.name)
+        elif field.default is not msgspec.NODEFAULT:
+            default = field.default
+        elif field.default_factory is not msgspec.NODEFAULT:
+            default = field.default_factory()
+        else:
+            default = MISSING_NONPROP
+
+        field_list.append(
+            StructFieldSpec(
+                name=field.name,
+                type=annotations[field.name],
+                default=default,
+                helptext=get_field_docstring(info.type, field.name, info.markers),
+            )
+        )
+
+    return StructConstructorSpec(instantiate=info.type, fields=tuple(field_list))
