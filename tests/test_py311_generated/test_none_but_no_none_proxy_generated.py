1c8a6e358e19b6d0e33abf35109c3a59535dfe15
Brent Yi <yibrenth@gmail.com>
2025-10-12 22:27:34 -0700

    Fix subcommand matching failure for `None` types with metadata (#357)
    * Fix subcommand matching failure for `None` types with metadata
    
    * Python 3.8
    
    * More Python 3.8

---
+++ tests/test_py311_generated/test_none_but_no_none_proxy_generated.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Annotated
+
+import tyro
+
+
+@dataclass
+class Dataclass:
+    a: int
+    b: str = "default"
+
+
+@dataclass
+class Config:
+    x: Annotated[
+        Dataclass | None,
+        tyro.conf.arg(
+            constructor=tyro.extras.subcommand_type_from_defaults(
+                {
+                    "none": None,
+                    "dc": Dataclass(3),
+                }
+            )
+        ),
+    ] = None
+
+
+def test_simple() -> None:
+    """Check for edge case where the `None` type in the union is annotated with
+    metadata."""
+    assert tyro.cli(Config, args=[]) is None
