3d28107b7674cf45dcd21397f14d42c87e3f7a9a
Brent Yi <yibrenth@gmail.com>
2025-11-06 18:17:23 -0800

    Optimize imports (#380)
    * Optimize imports
    
    * pragma: no cover
    
    ---------
    
    Signed-off-by: Brent Yi <yibrenth@gmail.com>

--- src/tyro/_backends/__init__.py
+++ src/tyro/_backends/__init__.py
@@ -3,6 +3,3 @@
 `TyroBackend` is faster and more flexible, and should be used by default. We
 don't have a public API for switching to the ArgparseBackend.
 """
-
-from ._argparse_backend import ArgparseBackend as ArgparseBackend
-from ._tyro_backend import TyroBackend as TyroBackend
