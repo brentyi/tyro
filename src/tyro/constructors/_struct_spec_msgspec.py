4c6a9bd58267b927b98484e1ecf9e13b23b91c63
brentyi <yibrenth@gmail.com>
2026-03-13 10:17:59 -0700

    Cleanup

--- src/tyro/constructors/_struct_spec_msgspec.py
+++ src/tyro/constructors/_struct_spec_msgspec.py
@@ -5,7 +5,7 @@ import sys
 
 from .._docstrings import get_field_docstring
 from .._resolver import get_type_hints_resolve_type_params
-from .._singleton import MISSING_NONPROP, is_missing
+from .._singleton import MISSING, MISSING_NONPROP
 from ._struct_spec import StructConstructorSpec, StructFieldSpec, StructTypeInfo
 
 
@@ -31,7 +31,10 @@ def msgspec_rule(info: StructTypeInfo) -> StructConstructorSpec | None:
     annotations = get_type_hints_resolve_type_params(info.type, include_extras=True)
 
     for field in struct_type.fields:
-        if not is_missing(info.default) and hasattr(info.default, field.name):
+        if info.default not in (
+            MISSING,
+            MISSING_NONPROP,
+        ):
             default = getattr(info.default, field.name)
         elif field.default is not msgspec.NODEFAULT:
             default = field.default
