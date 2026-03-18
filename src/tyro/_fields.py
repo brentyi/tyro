b7fbae7b5f81ebc7b821300bba12b585ae0341d5
Brent Yi <yibrenth@gmail.com>
2026-03-17 23:54:24 -0700

    Fix `tyro.conf.Positional` for `*args` (#441)
    * Fix `tyro.conf.Positional` for `*args`
    
    * Ruff formatting + test generation

--- src/tyro/_fields.py
+++ src/tyro/_fields.py
@@ -491,6 +491,13 @@ def _field_list_from_function(
             #
             # This will create a `--args T [T ...]` CLI argument.
             call_mode = "unpack_args"
+            # Extract markers from annotation before wrapping in Tuple,
+            # otherwise markers like Positional get buried inside the
+            # Tuple type and are lost.
+            _, extracted_metadata = _resolver.unwrap_annotated(typ, search_type="all")
+            func_markers = func_markers + tuple(
+                x for x in extracted_metadata if isinstance(x, _markers._Marker)
+            )
             typ = Tuple[(typ, ...)]  # type: ignore
             # Only set empty default when there's no default_instance.
             # When default_instance is provided, we want MISSING_NONPROP so that
