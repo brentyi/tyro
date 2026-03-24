218bf853001df068c1a359524eaacb966c5f26c0
mxmf <2993747433@qq.com>
2026-03-24 16:38:36 +0800

    fix import ordering for ruff
    Signed-off-by: mxmf <2993747433@qq.com>

--- src/tyro/_backends/_completion/__init__.py
+++ src/tyro/_backends/_completion/__init__.py
@@ -4,8 +4,8 @@ from __future__ import annotations
 
 from ._base import CompletionGenerator
 from ._tyro_bash import TyroBashCompletionGenerator
-from ._tyro_zsh import TyroZshCompletionGenerator
 from ._tyro_fish import TyroFishCompletionGenerator
+from ._tyro_zsh import TyroZshCompletionGenerator
 
 __all__ = [
     "CompletionGenerator",
